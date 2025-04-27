3ea91183fc4a75d2de3fea087a3427d9d8d1aed8
nahiefhspc <t.al.khackheist@gmail.com>
2025-04-27 21:17:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,10 +73,10 @@ image_list = [
 ]
 print(4321)
 #bot = Client(
-    "bot",
-    api_id=api_id,
-    api_hash=api_hash,
-    bot_token=bot_token)
+#    "bot",
+#    api_id=api_id,
+#    api_hash=api_hash,
+ #   bot_token=bot_token)
 
 @bot.on_message(filters.command(["start"]))
 async def start(bot, message):
